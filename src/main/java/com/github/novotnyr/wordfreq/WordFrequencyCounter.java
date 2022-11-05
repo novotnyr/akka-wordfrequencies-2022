b4eafe8e9571f7a1079a814aed93d58ea2027aa2
Robert Novotny <novotnr0@gmail.com>
2022-11-05 13:56:55 +0100

    Pridane zakladne sucasti aktora - tvorba a obsluha sprav
    Aktor:
    
    - dedi od AbstractBehavior
    - musi zdedit privatny konstruktor
    - pripravit metodu create() na inicializaciu
    - pridat obsluhu sprav

--- src/main/java/com/github/novotnyr/wordfreq/WordFrequencyCounter.java
+++ src/main/java/com/github/novotnyr/wordfreq/WordFrequencyCounter.java
@@ -1,6 +1,37 @@
 package com.github.novotnyr.wordfreq;
 
-public class WordFrequencyCounter {
+ import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+ import static com.github.novotnyr.wordfreq.Utils.getWordFrequencies;
+ import static com.github.novotnyr.wordfreq.WordFrequencyCounter.Command;
+
+public class WordFrequencyCounter extends AbstractBehavior<Command> {
+
+    private WordFrequencyCounter(ActorContext<Command> context) {
+        super(context);
+    }
+
+    public static Behavior<Command> create() {
+        return Behaviors.setup(WordFrequencyCounter::new);
+    }
+
+    @Override
+    public Receive<Command> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(CountWordFrequencies.class, this::countWordFrequencies)
+                .build();
+    }
+
+    private Behavior<Command> countWordFrequencies(CountWordFrequencies command) {
+        String sentence = command.sentence();
+        getContext().getLog().debug("Sentence '{}': {}", sentence, getWordFrequencies(sentence));
+        return Behaviors.same();
+    }
+
     public interface Command {
     }
 
